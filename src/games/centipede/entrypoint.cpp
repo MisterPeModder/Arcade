fb733c63609ab3f7405e6f6b46c6e1289dc1492f
Yanis Guaye <yanis.guaye@epitech.eu>
2022-03-22 04:35:47 +0100

    Logo Demo: Add title, texture coloring and pausing

--- src/games/centipede/entrypoint.cpp
+++ src/games/centipede/entrypoint.cpp
@@ -21,9 +21,16 @@ namespace arcade
         {
             this->_display = &display;
             this->_logoTexture = display.loadAsset("./doc/logo.png", IAsset::Type::Texture);
-            this->_logo = display.createRectObject({200, 200}, this->_logoTexture.get());
+            this->_font = display.loadAsset("./assets/fonts/roboto_regular.ttf", IAsset::Type::Font);
+            this->_logo = display.createRectObject({27, 27}, this->_logoTexture.get());
+            this->_title = display.createTextObject("This is a test", this->_font.get());
             this->_dragging = false;
             this->_state = State::Loaded;
+
+            this->_logo->setBackground(Color(0x221111));
+            this->_logo->setForeground(Color(0xccccff));
+            this->_title->setForeground(Color(0x2a9d8f));
+            this->_title->setBackground(Color::Green);
         }
 
         void setDisplay(IDisplay &display) override final
@@ -35,6 +42,8 @@ namespace arcade
         {
             this->_logo.reset();
             this->_logoTexture.reset();
+            this->_font.reset();
+            this->_title.reset();
         }
 
         void setState(State state) override final
@@ -61,12 +70,24 @@ namespace arcade
         void draw() override final
         {
             this->_display->drawGameObject(*this->_logo);
+            this->_display->drawGameObject(*this->_title);
         }
 
         void handleEvent(Event &event) override final
         {
-            if (event.type == Event::Event::Type::Resized
-                || (event.type == Event::Event::Type::KeyPressed && event.key.code == 'c')) {
+            if (event.type == Event::Type::KeyReleased) {
+                if (event.key.code == '\x1b')
+                    this->_state = State::Ended;
+                else if (event.key.code == 'p')
+                    this->_state = this->_state == State::Paused ? State::Running : State::Paused;
+            }
+
+            // Don't update anything else if the game is paused
+            if (this->_state == State::Paused)
+                return;
+
+            if (event.type == Event::Type::Resized
+                || (event.type == Event::Type::KeyPressed && event.key.code == 'c')) {
                 vec2u size(this->_logo->getSize());
                 vec2u winSize = this->_display->getSize();
                 vec2i pos{static_cast<int>(winSize.x) / 2 - static_cast<int>(size.x) / 2,
@@ -74,11 +95,12 @@ namespace arcade
 
                 this->_logo->setPosition(pos);
                 this->_dragging = false;
-            } else if (event.type == Event::Event::Type::MouseButtonPressed) {
+            } else if (event.type == Event::Type::MouseButtonPressed) {
                 this->_dragging = true;
-            } else if (event.type == Event::Event::Type::MouseButtonReleased) {
+            } else if (event.type == Event::Type::MouseButtonReleased) {
+                std::cout << "released at " << event.mouseButton.pos.x << ", " << event.mouseButton.pos.y << std::endl;
                 this->_dragging = false;
-            } else if (event.type == Event::Event::Type::MouseMoved && this->_dragging) {
+            } else if (event.type == Event::Type::MouseMoved && this->_dragging) {
                 vec2u size(this->_logo->getSize());
                 vec2i mousePos(event.mouseMove.pos);
                 vec2i pos{mousePos.x - static_cast<int>(size.x) / 2, mousePos.y - static_cast<int>(size.y) / 2};
@@ -92,7 +114,9 @@ namespace arcade
         IDisplay *_display;
 
         std::unique_ptr<IAsset> _logoTexture;
+        std::unique_ptr<IAsset> _font;
         std::unique_ptr<IGameObject> _logo;
+        std::unique_ptr<IGameObject> _title;
 
         bool _dragging;
     };
